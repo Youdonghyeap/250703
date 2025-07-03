f6607d18a517e304edf3aeb2dfd8f912e5ca0c52
DESKTOP-B3EL7F1\202_27 <backjung0823@naver.com>
2025-07-03 14:33:56 +0900

    총알 발사

--- Assets/Scenes/script/Move.cs
+++ Assets/Scenes/script/Move.cs
@@ -7,6 +7,8 @@ using UnityEngine.InputSystem;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public Transform childTransform;
+    public GameObject bulletPrefab; // Inspector에서 큐브 프리팹 할당
+    public float bulletSpeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +22,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 마우스 좌클릭 시 바라보는 방향으로 큐브(총알) 발사
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if (bulletPrefab != null && childTransform != null)
+            {
+                GameObject bullet = Instantiate(bulletPrefab, childTransform.position, childTransform.rotation);
+                Rigidbody rb = bullet.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    // childTransform의 로컬 y축 방향으로 발사
+                    rb.linearVelocity = childTransform.up * bulletSpeed;
+                }
+                // 3초 뒤에 bullet 오브젝트 파괴
+                Destroy(bullet, 3f);
+            }
+        }
         if (Keyboard.current.upArrowKey.isPressed)
         {
             transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime);
